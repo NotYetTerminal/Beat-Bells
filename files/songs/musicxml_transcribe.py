1f4d80898c77a0c4958c7aaa3192ad110ca4491c
Gabor Major <113538041+C00271548@users.noreply.github.com>
2023-01-26 18:34:26 +0000

    Transcribe works ;( finally

--- files/songs/musicxml_transcribe.py
+++ files/songs/musicxml_transcribe.py
@@ -1,12 +1,13 @@
 import xml.etree.ElementTree as ET
 
-file: str = 'songs/Troupe Master Grimm'
+file: str = 'songs/Mantis Lords'
 tree = ET.parse(file + '.musicxml')
 music_data = ''
 
 for element in tree.getroot():
     if element.tag == 'part':
         music_data = element
+        break
     print(element.tag, element.attrib)
 
 if music_data == '':
@@ -23,28 +24,89 @@ note_data_dict: dict = {'step': '', # capital letter
                         'octave': '', # int
                         'alter': '0', # -2 <-> 2
                         'type': 0, # number of beats
-                        'beat_count': 0} # beat number int
+                        'default-x': 0,
+                        'staff': '0'} # beat number int
+
+nothing_note_data_dict: dict = {'step': '',
+                                'octave': '',
+                                'alter': '0',
+                                'duration': 0,
+                                'default-x': -1,
+                                'staff': '0'}
 
 lowest_duration: float = 64.0
 durations: list = []
 types: list = []
+full_music_data: list = []
 
-for bar in music_data:
-    for item in bar:
+# duration_dict: dict = {'2': 1,
+#                        '3': 2,
+#                        '4': 3,
+#                        '6': 4,
+#                        '12': 8,
+#                        '24': 16}
+                       
+duration_dict: dict = {'1': 1,
+                       '2': 2,
+                       '4': 4,
+                       '6': 6,
+                       '8': 8,
+                       '12': 12,
+                       '16': 16}
+
+for bar1 in music_data:
+    for item in bar1:
         if item.tag == 'note':
+            note_data_dict = {'step': 'R',
+                              'octave': '',
+                              'alter': '0',
+                              'duration': 0,
+                              'default-x': -1,
+                              'staff': '0'}
+            if 'default-x' in item.attrib:
+                note_data_dict['default-x'] = item.attrib['default-x']
+            
             for data in item:
+                if data.tag == 'pitch':
+                    for data2 in data:
+                        note_data_dict[data2.tag] = data2.text
                 if data.tag == 'duration':
+                    if data.text not in duration_dict.keys():
+                        print('Key Error')
+                        print(data.text)
+                        input()
+                    note_data_dict[data.tag] = duration_dict[data.text]
+
+                    # durations stuff
                     if int(data.text) < lowest_duration:
                         lowest_duration = int(data.text)
                         print(item[1].text)
                         print(item[3].text)
-                    
                     for data2 in item:
                         if data2.tag == 'type':
                             types.append(data2.text)
                             durations.append(data.text)
                             break
+                if data.tag == 'staff':
+                    note_data_dict['staff'] = data.text
+                if data.tag == 'notations' and data[0].attrib['type'] == 'stop' and note_data_dict['duration'] != 1:
+                    print(note_data_dict)
+                    note_data_dict['step'] = 'R'
+                    note_data_dict['octave'] = '-1'
+                    note_data_dict['alter'] = '0'
+            
+            full_music_data.append(note_data_dict)
 
+        
+        # elif item.tag == 'backup':
+        #     for data in item:
+        #         if data.tag == 'duration':
+        #             full_music_data.append(int(data.text))
+    
+    full_music_data.append(nothing_note_data_dict)
+
+
+full_music_data.append(nothing_note_data_dict)
 
 temp_dict: dict = {}
 print(len(durations))
@@ -58,86 +120,78 @@ for index in range(len(types)):
 print(lowest_duration)
 print(temp_dict)
 
-duration_dict: dict = {'2': 1,
-                       '3': 2,
-                       '4': 3,
-                       '6': 4,
-                       '12': 8,
-                       '24': 16}
-
-# duration_dict: dict = {'1': 1,
-#                        '2': 2,
-#                        '4': 4,
-#                        '6': 6,
-#                        '8': 8,
-#                        '16': 16}
-
-beat_bar_sum: int = 0
+treble_index: int = 0
 beat_count: int = 0
-for bar in music_data:
-    treble_bar_list = []
-    bass_bar_list = []
-    beat_bar_sum = 0
-    print('Start of bar:', (beat_count + 2))
-    for item in bar:
-        if item.tag == 'note':
-            note_data_dict = {'step': '',
-                              'octave': '',
-                              'alter': '0',
-                              'duration': 0,
-                              'beat_count': 0}
-            for data in item:
-                if data.tag == 'pitch':
-                    for data2 in data:
-                        note_data_dict[data2.tag] = data2.text
-                if data.tag == 'duration':
-                    if data.text not in duration_dict.keys():
-                        print('Key Error')
-                        print(data.text)
-                        input()
-                    note_data_dict[data.tag] = duration_dict[data.text]
+for note_index in range(len(full_music_data) - 1):
+    note = full_music_data[note_index]
+    if note['staff'] == '1':
+        if full_music_data[note_index + 1]['staff'] == '1' and abs(float(full_music_data[note_index + 1]['default-x']) - float(note['default-x'])) <= 15:
+            note['default-x'] = 0
+        treble_line.append(note)
+        treble_index += 1
+    elif note['staff'] == '2':
+        if full_music_data[note_index + 1]['staff'] == '2' and abs(float(full_music_data[note_index + 1]['default-x']) - float(note['default-x'])) <= 15:
+            note['default-x'] = 0
+        bass_line.append(note)
+#quit()
+    #     if item.tag == 'note':
+    #         note_data_dict = {'step': '',
+    #                           'octave': '',
+    #                           'alter': '0',
+    #                           'duration': 0,
+    #                           'beat_count': 0}
+    #         for data in item:
+    #             if data.tag == 'pitch':
+    #                 for data2 in data:
+    #                     note_data_dict[data2.tag] = data2.text
+    #             if data.tag == 'duration':
+    #                 if data.text not in duration_dict.keys():
+    #                     print('Key Error')
+    #                     print(data.text)
+    #                     input()
+    #                 note_data_dict[data.tag] = duration_dict[data.text]
                 
-                if data.tag == 'staff':
-                    if beat_count >= 174:
-                        print(previous_beat_duration)
-                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah!
-                    if len(item.attrib.keys()) != 0 and item.attrib['default-x'] != previous_note_x_data and (previous_note_x_data == '' or abs(float(item.attrib['default-x']) - float(previous_note_x_data)) > 12):
-                        beat_count += previous_beat_duration
-                        beat_bar_sum += previous_beat_duration
-                        previous_beat_duration = note_data_dict['duration']
-                        previous_note_x_data = item.attrib['default-x']
-                    elif len(item.attrib.keys()) == 0:
-                        beat_count += previous_beat_duration
-                        beat_bar_sum += previous_beat_duration
-                        previous_beat_duration = note_data_dict['duration']
-                        previous_note_x_data = ''
-                    else:
-                        if abs(float(item.attrib['default-x']) - float(previous_note_x_data)) < 12 and abs(float(item.attrib['default-x']) - float(previous_note_x_data)) > 1:
-                            print('Double note on line:', float(previous_note_x_data), float(item.attrib['default-x']))
+    #             if data.tag == 'staff':
+    #                 if beat_count >= 174:
+    #                     print(previous_beat_duration)
+    #                     #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah!
+    #                 if len(item.attrib.keys()) != 0 and item.attrib['default-x'] != previous_note_x_data and (previous_note_x_data == '' or abs(float(item.attrib['default-x']) - float(previous_note_x_data)) > 12):
+    #                     beat_count += previous_beat_duration
+    #                     beat_bar_sum += previous_beat_duration
+    #                     previous_beat_duration = note_data_dict['duration']
+    #                     previous_note_x_data = item.attrib['default-x']
+    #                 elif len(item.attrib.keys()) == 0:
+    #                     beat_count += previous_beat_duration
+    #                     beat_bar_sum += previous_beat_duration
+    #                     previous_beat_duration = note_data_dict['duration']
+    #                     previous_note_x_data = ''
+    #                 else:
+    #                     if abs(float(item.attrib['default-x']) - float(previous_note_x_data)) < 12 and abs(float(item.attrib['default-x']) - float(previous_note_x_data)) > 1:
+    #                         print('Double note on line:', float(previous_note_x_data), float(item.attrib['default-x']))
 
-                    note_data_dict['beat_count'] = beat_count
+    #                 note_data_dict['beat_count'] = beat_count
 
-                    if data.text == '1':
-                        treble_bar_list.append(note_data_dict)
-                    elif data.text == '2':
-                        bass_bar_list.append(note_data_dict)
-                    else:
-                        print('Error')
-                        print(data.text)
-                        input()
+    #                 if data.text == '1':
+    #                     treble_bar_list.append(note_data_dict)
+    #                 elif data.text == '2':
+    #                     bass_bar_list.append(note_data_dict)
+    #                 else:
+    #                     print('Error')
+    #                     print(data.text)
+    #                     input()
         
-        elif item.tag == 'backup':
-            for data in item:
-                if data.tag == 'duration':
-                    if data.text not in duration_dict.keys():
-                        print('Key Error')
-                        print(data.text)
-                        input()
-                    beat_count -= duration_dict[data.text]
+    #     elif item.tag == 'backup':
+    #         for data in item:
+    #             if data.tag == 'duration':
+    #                 if data.text not in duration_dict.keys():
+    #                     print('Key Error')
+    #                     print(data.text)
+    #                     input()
+    #                 beat_count -= duration_dict[data.text]
     
-    treble_line.append(treble_bar_list)
-    bass_line.append(bass_bar_list)
-    print('Sum of bar:', beat_bar_sum)
+    # treble_line.append(treble_bar_list)
+    # bass_line.append(bass_bar_list)
+    # print('Sum of bar:', beat_bar_sum)
 
 
 def convert_duration_amount(dur_in: int) -> str:
@@ -152,44 +206,66 @@ def convert_duration_amount(dur_in: int) -> str:
 # second is the note frequency number from 0 to 8
 # third is the note frequency character from a to g
 # lastly is whether it is a neutral or sharp, n/s
-def export_music_data(file_name: str, music_data: list):
+def export_music_data(file_name: str, in_music_data: list):
     exported_music_data: list = []
     note_string: str = ''
-    previous_beat_count: int = -1
+    index: int = 0
 
-    for bar in music_data:
-        for note in bar:
-            if note['step'] != '':
-                #print(note)
-                note_string = convert_duration_amount(note['duration']) + str(note['octave']) + note['step'].lower()
-                if note['alter'] == '1':
-                    note_string += 's'
-                elif note['alter'] == '-1':
-                    note['step'] = chr(ord(note['step']) - 1)
-                    if note['step'] == '@':
-                        note['step'] = 'G'
-                    note_string = convert_duration_amount(note['duration']) + str(note['octave']) + note['step'].lower()
-                    if note['step'] != 'B' and note['step'] != 'E':
-                        note_string += 's'
+    for note in in_music_data:
+        if index < 50:
+            print(note, index)
+        note_string = convert_duration_amount(note['duration']) + str(note['octave']) + note['step'].lower()
+        if note['alter'] == '1':
+            note_string += 's'
+        elif note['alter'] == '-1':
+            note['step'] = chr(ord(note['step']) - 1)
+            if note['step'] == '@':
+                note['step'] = 'G'
+            note_string = convert_duration_amount(note['duration']) + str(note['octave']) + note['step'].lower()
+            if note['step'] != 'B' and note['step'] != 'E':
+                note_string += 's'
+            else:
+                note_string += 'n'
+        elif note['alter'] != '0':
+            print('Error')
+            print(note)
+            input()
+        else:
+            note_string += 'n'
+        
+        if index < 50:
+            print(note_string)
+        if note['step'] != '':
+            if note['step'] != 'R':
+                if note['default-x'] == 0:
+                    if len(exported_music_data) == 0 or in_music_data[index - 1]['default-x'] != 0:
+                        exported_music_data.append(note_string)
                     else:
-                        note_string += 'n'
-                elif note['alter'] != '0':
-                    print('Error')
-                    print(note)
-                    input()
+                        exported_music_data[-1] += note_string
                 else:
-                    note_string += 'n'
-                
-                if previous_beat_count == note['beat_count']:
-                    exported_music_data[-1] += note_string
-                else:
-                    #print(note['beat_count'] - previous_beat_count - 1)
-                    for _ in range(note['beat_count'] - previous_beat_count - 1):
-                    #for _ in range(note_duration - 1):
+                    if len(exported_music_data) == 0 or exported_music_data[-1] == '0' or in_music_data[index - 1]['default-x'] != 0:
+                        exported_music_data.append(note_string)
+                    else:
+                        exported_music_data[-1] += note_string
+                    for _ in range(note['duration'] - 1):
                         exported_music_data.append('0')
-                    
-                    exported_music_data.append(note_string)
-                    previous_beat_count = note['beat_count']
+            else:
+                if note['octave'] == '-1':
+                    note['duration'] -= 1
+                for _ in range(note['duration']):
+                    exported_music_data.append('0')
+            
+        # if previous_beat_count == note['beat_count']:
+        #     exported_music_data[-1] += note_string
+        # else:
+        #     #print(note['beat_count'] - previous_beat_count - 1)
+        #     for _ in range(note['beat_count'] - previous_beat_count - 1):
+        #     #for _ in range(note_duration - 1):
+        #         exported_music_data.append('0')
+    
+        #     exported_music_data.append(note_string)
+        #     previous_beat_count = note['beat_count']
+        index += 1
 
 
     for _ in range(16 - (len(exported_music_data) % 16)):
@@ -201,15 +277,15 @@ def export_music_data(file_name: str, music_data: list):
         end_index: int = 4
         for start_index in range(0, len(exported_music_data), 4):
             f.write(','.join(exported_music_data[start_index:end_index]) + ',\n')
-            end_index += 4
         
-            if end_index % 16 == 4:
-                f.write('\n')
+            #if end_index % 16 == 0:
+            #    f.write('\n')
+            end_index += 4
             
         f.write('0,0,0,0,\n0,0,0,0,\n0,0,0,0,\n0,0,0,0,\n')
         f.write('0,0,0,0,\n0,0,0,0,\n0,0,0,0,\n0,0,0,0')
 
 
-export_music_data(file + ' T', treble_line)
-export_music_data(file + ' B', bass_line)
+export_music_data(file + ' T', treble_line[:])
+#export_music_data(file + ' B', bass_line)
 
