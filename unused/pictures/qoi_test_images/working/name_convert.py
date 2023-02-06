14b581e24bdd8c780ad36d9226f54484d935edbd
Gabor Major <113538041+C00271548@users.noreply.github.com>
2023-02-06 13:12:51 +0000

    QOI Decode put in

---
+++ unused/pictures/qoi_test_images/working/name_convert.py
@@ -0,0 +1,10 @@
+import os
+
+base_path: str = 'C:/Users/gabor.MCS/Documents/GitHub/68k-project/unused/pictures/qoi_test_images/working/'
+
+index: int = 1
+name: str = input('Name plus number: ')
+for file in os.listdir(base_path):
+    if file.endswith('.png'):
+        os.rename(base_path + file, base_path + name + str(index) + '.png')
+        index += 1
