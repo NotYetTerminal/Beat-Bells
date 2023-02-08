e14562a17b19aae0c3ad55171486815e8c9ae6c6
Gabor Major <113538041+C00271548@users.noreply.github.com>
2023-02-08 14:52:00 +0000

    Character sprite gif works LETS GOOOOOOOOOOOOO!!!!

--- unused/pictures/qoi_test_images/working/name_convert.py
+++ unused/pictures/qoi_test_images/working/name_convert.py
@@ -2,7 +2,7 @@ import os
 
 base_path: str = 'C:/Users/gabor.MCS/Documents/GitHub/68k-project/unused/pictures/qoi_test_images/working/'
 
-index: int = 1
+index: int = 0
 name: str = input('Name plus number: ')
 for file in os.listdir(base_path):
     if file.endswith('.png'):
