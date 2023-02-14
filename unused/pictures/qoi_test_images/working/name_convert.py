5804353b152e91875ce8d9d8a3ebc87a35c1d577
Gabor Major <113538041+C00271548@users.noreply.github.com>
2023-02-14 15:49:16 +0000

    Smoothed input and changed unfilled notes

--- unused/pictures/qoi_test_images/working/name_convert.py
+++ unused/pictures/qoi_test_images/working/name_convert.py
@@ -5,6 +5,8 @@ base_path: str = 'C:/Users/gabor.MCS/Documents/GitHub/68k-project/unused/picture
 index: int = 0
 name: str = input('Name plus number: ')
 for file in os.listdir(base_path):
+    if file.startswith('a') or file.startswith('b'):
+        continue
     if file.endswith('.png'):
         os.rename(base_path + file, base_path + name + str(index) + '.png')
         index += 1
